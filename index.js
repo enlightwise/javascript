a7d728b9f4427990e89b562765ce8a4e8a4f16f8
enlightwise <enlightwise@protonmail.com>
2025-01-25 22:36:33 +0100

    operators

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ function greet(name, lastName) {
     console.log('hello ' + name + ' ' + lastName)
 }
 
+function square(number) {
+    return number * number;
+}
+
 greet('John', 'Smith');
 
+let number = square(2);
+console.log(number);
+
+console.log(number++)
+console.log(++number)
+
+console.log(number == '6')
+console.log(number === '6')
