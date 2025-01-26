16fc8fa2894df75d988e2a8c718ddad09ab10136
enlightwise <enlightwise@protonmail.com>
2025-01-26 12:18:27 +0100

    constructor function

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ console.log(circle1);
 
 const circle2 = createCircle(1);
 console.log(circle2);
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+    // return this;
+}
+
+const circle = new Circle(1);
+circle.draw();
