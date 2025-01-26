7b14560762dcf46b8a76d4658a6efff83e945e34
enlightwise <enlightwise@protonmail.com>
2025-01-26 17:13:23 +0100

    spread operator

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const first = [1, 2, 3];
 const second = [4, 5, 6];
 
 
-// combine
-const combined = first.concat(second);
+// combine - spread operator
+const combined = [...first, 'a', ...second]
 // slice
-const slice = combined.slice(2, 4);
+// const copy = combined.slice(2, 4);
+const copy = [...combined];
 
 console.log(combined);
-console.log(slice);
+console.log(copy);
+
