c8639001d22658687c439c844221808f160898b6
enlightwise <enlightwise@protonmail.com>
2025-01-26 12:42:24 +0100

    value vs reference types

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-// functions are objects
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
+// let x = 10;
+// let y = x;
+
+// x = 20;
+
+let x = {value: 10};
+let y = x; // reference/address is copied, not value
+
+x.value = 20;
+
+// primities are copied by their value
+// objects are copied by their reference
+
+// let number = 10;
+
+// function increase(number){
+//     number++;
+// }
+
+// increase(number);
+// console.log(number);
+
+let obj = {value: 10};
+
+function increase(obj){
+    obj.value++;
 }
 
-const circle = new Circle(1);
-console.log(Circle.name);
-console.log(Circle.length);
-
-// new String();
-// new Boolean();
-// new Number();
+increase(obj);
+console.log(obj);
